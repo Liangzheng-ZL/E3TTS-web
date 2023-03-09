04e278803fbad1f0f9c87201e1a2c7b70d2d97fe
Liangzheng-ZL <liangzhenglz@sjtu.edu.cn>
2023-03-09 19:55:20 +0800

    add samples

--- insertion.py
+++ insertion.py
@@ -23,7 +23,7 @@ def section_insert_compare():
             e_text=e_text,
             audio_files=[
                 f"./samples/insert/{language}/{length}/{sys}/{name}.wav" for sys in (
-                    "gt", "baseline1", "baseline2", "baeline3", "bedit-tts",
+                    "gt", "baseline1", "baseline2", "baseline3", "bedit-tts",
                 )
             ],
             titles=[
