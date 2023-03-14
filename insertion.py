e295a83db91f493e384c741b1f4cdd10b737d259
Liangzheng-ZL <liangzhenglz@sjtu.edu.cn>
2023-03-14 19:24:20 +0800

    add length samples

--- insertion.py
+++ insertion.py
@@ -58,4 +58,29 @@ def section_insert_compare():
         "Edited text: " + """You are no longer in the <u><em><strong>middle</strong></em></u> midst of broken desolate wastes.""",
         "11697_actressinhighlife_01_bowen_0027"
     ))
+    blocks.append(_gen_table("short", "chn",
+        "Original text: " + """梁家仁出演的电视剧有什么。""",
+        "Edited text: " + """梁家仁<u><em><strong>曾经</strong></em></u>出演的电视剧有什么。""",
+        "SSB00180003"
+    ))
+    blocks.append(_gen_table("short", "chn",
+        "Original text: " + """来一首故乡。""",
+        "Edited text: " + """来一首<u><em><strong>关于</strong></em></u>故乡。""",
+        "SSB01120400"
+    ))
+    blocks.append(_gen_table("short", "chn",
+        "Original text: " + """又被电梯外门夹住头和身子。""",
+        "Edited text: " + """又被<u><em><strong>突然关闭的</strong></em></u>电梯外门夹住头和身子。""",
+        "SSB07370462"
+    ))
+    blocks.append(_gen_table("short", "chn",
+        "Original text: " + """河南平顶山化工厂氨气泄漏小孩儿咳血动物瘫倒。""",
+        "Edited text: " + """河南平顶山化工厂<u><em><strong>发生</strong></em></u>氨气泄漏小孩儿咳血动物瘫倒。""",
+        "SSB04270111"
+    ))
+    blocks.append(_gen_table("short", "chn",
+        "Original text: " + """之后将学校告上了法庭。""",
+        "Edited text: " + """之后将<u><em><strong>自己的</strong></em></u>学校告上了法庭。""",
+        "SSB12180020"
+    ))
    
